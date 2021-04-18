fa8df9ef762a6e477aa397f16a3f06e1e6d012aa
ruvaldak <truvaldak@gmail.com>
2021-04-18 13:27:09 -0400

    replace curl with invoke-webrequest
    remove the need for coreutils or curl. This means this program is only compatible with Windows 7 and up. If you're using anything below Windows 10, though, seriously, just upgrade.

--- _update_addons.bat
+++ _update_addons.bat
@@ -1,33 +1,6 @@
 @echo off
 SETLOCAL
 
-powershell -Command "$version = 'v1.0.2'; $response = Invoke-WebRequest https://api.github.com/repos/ruvaldak/Gw2AddonUpdater/releases/latest; $json = $response.Content | ConvertFrom-Json; $url = $json.assets[0].browser_download_Url; $latest = $json.tag_name; IF ($version -ne $latest) { Invoke-WebRequest $url -OutFile _update_addons.bat.upd }"
-IF EXIST "_update_addons.bat.upd" (
-	del /f _update_addons.bat
-	rename _update_addons.bat.upd _update_addons.bat
-	echo GW2AddonUpdater has been updated! Press ENTER to restart the program.
-	pause >nul
-	start cmd /c "%cd%/_update_addons.bat" && exit
-)
-
-
-where /q curl
-IF %ERRORLEVEL% NEQ 0 (
-	IF EXIST "%cd%/curl/curl-7.76.1-win64-mingw/bin/curl.exe" (
-		SET CURL="%cd%/curl/curl-7.76.1-win64-mingw/bin/curl.exe"
-		GOTO MENU
-	) ELSE (
-		powershell -Command "Invoke-WebRequest https://curl.se/windows/dl-7.76.1/curl-7.76.1-win64-mingw.zip -OutFile curl.zip"
-		powershell Expand-Archive curl.zip
-		del /f curl.zip
-		SET CURL="%cd%/curl/curl-7.76.1-win64-mingw/bin/curl.exe"
-		GOTO MENU
-	)
-) ELSE (
-	SET CURL=curl
-	GOTO MENU
-)
-
 :MENU
 cd bin64
 ECHO.
@@ -39,15 +12,19 @@ ECHO.
 ECHO 1 - Update/Install Both (ArcDPS+d912pxy)
 ECHO 2 - Update/Install ArcDPS
 ECHO 3 - Update/Install d912pxy
-ECHO 4 - I'm having issues! (troubleshooting menu)
-ECHO 5 - EXIT
+ECHO 4 - Other addons
+ECHO 5 - I'm having issues! (troubleshooting menu)
+ECHO 6 - Settings menu
+ECHO 0 - EXIT
 ECHO.
 SET /P M=Input: 
 IF %M%==1 GOTO QUICK
 IF %M%==2 GOTO ARC
 IF %M%==3 GOTO PXY
-IF %M%==4 GOTO TROUBLE
-IF %M%==5 GOTO EOF
+IF %M%==4 GOTO EXTRA
+IF %M%==5 GOTO TROUBLE
+IF %M%==6 GOTO SETTINGS
+IF %M%==0 GOTO EOF
 
 :QUICK
 GOTO PXY
@@ -60,7 +37,7 @@ echo Backing up current ArcDPS...
 rename d3d9.dll d3d9.dll.bak
 )
 echo Downloading new ArcDPS...
-%CURL% --output d3d9.dll --url https://www.deltaconnected.com/arcdps/x64/d3d9.dll
+powershell -Command "Invoke-WebRequest https://www.deltaconnected.com/arcdps/x64/d3d9.dll -OutFile d3d9.dll"
 echo Done!
 GOTO MENU
 
@@ -83,16 +60,11 @@ xcopy.exe /s /y d912pxy d912pxy.bak
 echo Making temporary working directory...
 mkdir tmp
 cd ./tmp/
-echo Downloading some tools...
-%CURL% -Lk --output coreutils.zip --url "http://download-mirror.savannah.gnu.org/releases/coreutils/windows-64bit-unsupported/coreutils-8.31-28-windows-64bit.zip"
-powershell Expand-Archive coreutils.zip -DestinationPath coreutils
-del /f coreutils.zip
-%CURL% -s https://api.github.com/repos/megai2/d912pxy/releases/latest \ | findstr browser_download_url | "./coreutils/coreutils-8.31-28-windows-64bit/cut.exe" -d : -f 2,3 > url.txt && set /p url=<url.txt 
-SET url=%url:"=% && %CURL% -Lk --output d912pxy.zip --url %url% && del /f url.txt
-rmdir /s /q coreutils
-powershell Expand-Archive d912pxy.zip -DestinationPath ./
+echo Downloading and extracting d912pxy...
+powershell -Command "$response = Invoke-WebRequest https://api.github.com/repos/megai2/d912pxy/releases/latest; $json = $response.Content | ConvertFrom-Json; $url = $json.assets[0].browser_download_Url; Invoke-WebRequest $url -OutFile d912pxy.zip; Expand-Archive d912pxy.zip -DestinationPath ./"
 del /f d912pxy.zip
 cd ..
+echo Copying extracted files...
 xcopy.exe /s /y /i "%cd%/tmp/d912pxy" "%cd%/d912pxy"
 rmdir /s /q tmp
 IF %M%==1 GOTO PXYARC
